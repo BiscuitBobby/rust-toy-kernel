948aa7daf083f51f1877a53eb092f2eef256da4b
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-06-06 11:10:51 +0530

    Marked the function as extern "C" to tell the compiler that it should use the C calling convention _start as _start is the default entry point name for most systems.

--- rusk_os/src/main.rs
+++ rusk_os/src/main.rs
@@ -1,9 +1,14 @@
 #![no_std] //disable link to standard library
-fn main() {
+#![no_main]
+#[no_mangle] // disable name mangling
+
+pub extern "C" fn _start() -> ! { //tell compiler that it should use the C calling convention for this function
+    loop {}
 }
 
 use core::panic::PanicInfo;
 
+// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
