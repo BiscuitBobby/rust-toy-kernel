8650ef9ef0357a9e05a64fd92139aee6f700e2a1
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-06-07 15:05:02 +0530

    added vga text buffer

--- rusk_os/src/main.rs
+++ rusk_os/src/main.rs
@@ -1,15 +1,26 @@
 #![no_std] //disable link to standard library
 #![no_main]
-#[no_mangle] // disable name mangling
-
-pub extern "C" fn _start() -> ! { //tell compiler that it should use the C calling convention for this function
-    loop {}
-}
 
 use core::panic::PanicInfo;
 
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! 
+{
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // disable name mangling
+pub extern "C" fn _start() -> ! { //tell compiler that it should use the C calling convention for this function
+    let vga_buffer = 0xb8000 as *mut u8; //buffer is located at address 0xb8000
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
