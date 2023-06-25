4ba5328a54516d8b43d9b7991e7c3eb4dc6df865
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-06-25 22:56:32 +0530

    smol changes

--- rusk_os/src/main.rs
+++ rusk_os/src/main.rs
@@ -1,3 +1,7 @@
+/*cargo build --target x86_64-rusk.json
+  cargo bootimage
+  qemu-system-x86_64 -drive format=raw,file=target/x86_64-rusk/debug/bootimage-crate_name.bin
+*/
 #![no_std] //disable link to standard library
 #![no_main]
 
@@ -10,11 +14,13 @@ fn panic(_info: &PanicInfo) -> !
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static HELLO: &[u8] = b"Boot succesful";
 
 #[no_mangle] // disable name mangling
 pub extern "C" fn _start() -> ! { //tell compiler that it should use the C calling convention for this function
     let vga_buffer = 0xb8000 as *mut u8; //buffer is located at address 0xb8000
+    let keyboard_status_port: u16 = 0x64;
+    let keyboard_data_port: u16 = 0x60;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
